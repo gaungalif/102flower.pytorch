3c6e665c72c0a4d72820f57d2c5caf690c43663f
gaungalif <gaungalif@gmail.com>
2021-09-02 18:06:57 +0800

    style: change class into function

--- flower/trainer/task.py
+++ flower/trainer/task.py
@@ -129,7 +129,7 @@ def train_network(epoch, tloader, vloader, net, criterion, optimizer, scheduler,
     global best_acc1
     # print('test')
     for ep in range(epoch):
-        if epoch == 5:
+        if epoch == 2:
             net.unfreeze()
             step_lr = getsteplr(base_lr=lr/100, max_lr=lr, step=6)
             print(step_lr)
@@ -147,6 +147,7 @@ def train_network(epoch, tloader, vloader, net, criterion, optimizer, scheduler,
                     {'params': net.resnet.fc.parameters(), 'lr': step_lr[4]}
                 ],
                 lr=step_lr[0])
+        print(optimizer)
         train_batch(ep, tloader, net, criterion, optimizer, log_freq=log_freq)
         valid_batch(vloader, net, criterion,  log_freq=log_freq)
         scheduler.step()
