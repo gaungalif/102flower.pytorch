95f4569ba0883cdca3d6233f0e584f2e819d0109
gaungalif <gaungalif@gmail.com>
2021-09-06 01:09:43 +0800

    feat: new load datasets class index function

--- predictor.py
+++ predictor.py
@@ -50,3 +50,20 @@ def load_checkpoint(filename):
 
 # model = load_flower_network(root.joinpath('model_best.pth'))
 # checkpoint = load_checkpoint(root.joinpath('checkpoint.pth'))
+
+def get_all_flower_names():
+    with open('./dataset/cat_to_name.json', 'r') as f:
+            cat_to_name = json.load(f)
+    return cat_to_name
+
+def flower_name(val, array_index=False):
+    labels = get_all_flower_names()
+    if array_index:
+        val = val + 1
+    return labels[str(val)]
+
+def getFlowerClassIndex(classes, class_to_idx):
+    idx_to_class = {val: key for key, val in class_to_idx.items()}
+    class_to_flower_class_idx = [idx_to_class[lab] for lab in classes.squeeze().numpy().tolist()]
+    flower_class_to_name = [flower_name(cls_idx) for cls_idx in class_to_flower_class_idx]
+    return class_to_flower_class_idx, flower_class_to_name
