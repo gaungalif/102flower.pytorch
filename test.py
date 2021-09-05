beed71ceab9d8a0cc09898a85c886354a773b75c
gaungalif <gaungalif@gmail.com>
2021-09-06 06:33:57 +0800

    style: remove unnecesary code

--- test.py
+++ test.py
@@ -15,16 +15,10 @@ def predictors(image_path='dataset/valid/2/image_05094.jpg', weight_path='weight
   device = torch.device('cuda:0' if use_gpu and torch.cuda.is_available() else 'cpu')
 
   images = image_path
-  
-  print(type(images))
-  print(type(weight_path))
+
   model = load_flower_network(base_dir.joinpath(weight_path), device)
-  print(base_dir.joinpath(weight_path))
   probs, classes = predict(images, model)
   print(probs)
 
-def main():
-  fire.Fire(component=predictors)
-
 if __name__ == '__main__':
-  main()
+  fire.Fire(component=predictors)
