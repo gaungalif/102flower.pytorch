6dcdac09ae1785f13efca49f93e7f42c02e3e534
gaungalif <gaungalif@gmail.com>
2021-09-06 01:11:21 +0800

    feat: process and imshow functions

--- predictor.py
+++ predictor.py
@@ -67,3 +67,40 @@ def getFlowerClassIndex(classes, class_to_idx):
     class_to_flower_class_idx = [idx_to_class[lab] for lab in classes.squeeze().numpy().tolist()]
     flower_class_to_name = [flower_name(cls_idx) for cls_idx in class_to_flower_class_idx]
     return class_to_flower_class_idx, flower_class_to_name
+
+def process_image(image):
+    ''' Scales, crops, and normalizes a PIL image for a PyTorch model,
+        returns an Numpy array
+    '''
+    im = PIL.Image.open(image)
+    mean_val, std_val = [0.485, 0.456, 0.406], [0.229, 0.224, 0.225]
+    do_transforms =  transforms.Compose([
+        transforms.Resize(255),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean_val,std_val)
+    ])
+    im_tfmt = do_transforms(im)
+    im_add_batch = im_tfmt.view(1, im_tfmt.shape[0], im_tfmt.shape[1], im_tfmt.shape[2])
+    return im_add_batch
+
+def imshow(image, ax=None, title=None):
+    """Imshow for Tensor."""
+    if ax is None:
+        fig, ax = plt.subplots()
+    
+    # PyTorch tensors assume the color channel is the first dimension
+    # but matplotlib assumes is the third dimension
+    image = image.numpy().transpose((1, 2, 0))
+    
+    # Undo preprocessing
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    image = std * image + mean
+    
+    # Image needs to be clipped between 0 and 1 or it looks like noise when displayed
+    image = np.clip(image, 0, 1)
+    
+    ax.imshow(image)
+    
+    return ax
