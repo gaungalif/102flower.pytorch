32f5c72bd989e575fd49236c2561943400b052b1
gaungalif <gaungalif@gmail.com>
2021-09-06 02:35:49 +0800

    refactor: remove weights auto downloader

--- predictor.py
+++ predictor.py
@@ -32,8 +32,8 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 curr_dir = Path(curr_dir)
 root = curr_dir.joinpath('102flower.pytorch/flower/weights')
-utils.download_url('https://drive.google.com/file/d/1c6Dz5QVESdPPAvW0NUWuSnToVkT9fQ3Q/view?usp=sharing', root=root, filename='model_best.pth')
-utils.download_url('https://drive.google.com/file/d/16doe5f4YTLlGpFR9_0WQtjAvsPMlwxxJ/view?usp=sharing', root=root, filename='checkpoint.pth')
+# utils.download_url('https://drive.google.com/file/d/1c6Dz5QVESdPPAvW0NUWuSnToVkT9fQ3Q/view?usp=sharing', root=root, filename='model_best.pth')
+# utils.download_url('https://drive.google.com/file/d/16doe5f4YTLlGpFR9_0WQtjAvsPMlwxxJ/view?usp=sharing', root=root, filename='checkpoint.pth')
 
 def load_flower_network(filename):
     if os.path.isfile(filename): 
